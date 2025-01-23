20e4ad7c9d5674b594d0dc37037d0800ed2f14c3
Desai-Vedant <vedant27803@gmail.com>
2025-01-23 10:30:08 +0530

    Handled paths other than existing to redirect to existing path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,10 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import LoginPage from "../components/LoginPage";
 import RegisterPage from "../components/RegisterPage";
 import MenuAppBar from "../components/MenuAppBar";
@@ -32,6 +37,7 @@ function App() {
           </Route>
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
+          <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </Router>
     </div>
